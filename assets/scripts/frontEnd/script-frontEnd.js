cd7df67cdcc601491f2ad9b1dab5b3f9ebb79df1
Matthew Duvall <mgduvall@Matthews-MacBook-Pro.local>
2019-02-17 21:22:10 -0800

    update based on peer feedback

---
+++ assets/scripts/frontEnd/script-frontEnd.js
@@ -0,0 +1,18 @@
+function searchByPlayerName() {
+  var input = document.getElementById("searchTerms");
+  var table = document.getElementById("characterTable");
+  var searchText = input.value.toLowerCase();
+  var rows = table.getElementsByTagName("tr");
+
+  var column, i, columnText;
+  for (i = 0; i < rows.length; i++) {
+    column = rows[i].getElementsByClassName("playerLastName")[0];
+    if (column) {
+      columnText = column.textContent || column.innerText;
+      if (columnText.toLowerCase().indexOf(searchText) > -1)
+        rows[i].classList.remove("hideRow");
+      else
+        rows[i].classList.add("hideRow");
+    }
+  }
+}
